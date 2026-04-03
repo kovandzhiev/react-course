e68801ce17cba0484bb414e703fac8ca9c38534d
Plamen Kovandzhiev <Plamen.Kovandzhiev@digitall.com>
2026-04-03 17:17:49 +0300

    05-travel-list stats

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -28,7 +28,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -112,10 +112,17 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentige = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>📦You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        📦You have {numItems} items on your list, and you already packed{" "}
+        {numPacked} ({percentige} %)
+      </em>
     </footer>
   );
 }
